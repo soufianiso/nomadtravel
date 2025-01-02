92d5b0e9040325a76f54bf400b71149cd684dd62
soufianiso <soufiantamasi@gmail.com>
2025-01-02 16:34:08 +0100

    feat[watchlit]: add storage

--- services/watchlist/internal/storage/storage.go
+++ services/watchlist/internal/storage/storage.go
@@ -14,66 +14,19 @@ type Storage struct {
 
 
 type Store interface {
-	GetMovies(context.Context ,*moviespb.ListMoviesRequest) ([]*moviespb.Movie, error)
+	AddItemToWatchlist(int32, int32) error
 }
 
-func NewMoviesStorage(db *sql.DB) *Storage{
+func NewWatchlistStorage(db *sql.DB) *Storage {
 	return &Storage{db: db}
 }
 
+func (s *Storage) AddItemToWatchlist(movieId int32, userId int32) error {
+	query := "INSERT INTO watchlists VALUES ($1, $2)" 
 
-func(s *Storage) GetMovies(ctx context.Context , req *moviespb.ListMoviesRequest) ([]*moviespb.Movie, error){
-   // Validate pagination parameters
-    page := req.GetPage()
-    if page <= 0 {
-        page = 1
-    }
-
-    pageSize := req.GetPageSize()
-    if pageSize <= 0 {
-        pageSize = 24 // Default page size
-    }
-
-    offset := (page - 1) * pageSize
-
-	rows, err := s.db.QueryContext(ctx, 
-		`SELECT original_title, original_language, overview, release_date,  adult, poster_path 
-		FROM movies 
-		ORDER BY original_title 
-		LIMIT $1 OFFSET $2`, pageSize, offset) 
-
-	if err != nil{
-		return nil, err
+	_ , err := s.db.Exec(query , userId, movieId)
+	if err != nil {
+		return err
 	}
-
-    defer rows.Close()
-
-	var movies []*moviespb.Movie
-
-	for rows.Next(){
-		var movie moviespb.Movie
-		err := rows.Scan(&movie.OriginalTitle,
-			&movie.OriginalLanguage,
-			&movie.Overview,  
-			&movie.ReleaseDate,
-			&movie.Adult,
-			&movie.PosterPath) 
-		if err != nil {
-			return nil, err
-		}
-		movies = append(movies, &movie)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	if len(movies) == 0{
-
-		return nil , errors.New("page not been found")
-
-	}
-	// Check for errors during iteration
-
-
-	return  movies, nil
+	return nil
 }
-
